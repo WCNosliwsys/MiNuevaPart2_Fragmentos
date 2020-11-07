fcc93ecba88588a0936318302007a5751843412f
Wilson Callisaya <demarktec@gmail.com>
2020-11-06 22:22:45 -0500

    Enviando parametros desde un adaptador y recibiendo parametros en DetalleCorreo

--- app/src/main/java/com/ditec/minuevapart2_fragmentos/DetalleCorreo.java
+++ app/src/main/java/com/ditec/minuevapart2_fragmentos/DetalleCorreo.java
@@ -3,12 +3,18 @@ package com.ditec.minuevapart2_fragmentos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetalleCorreo extends AppCompatActivity {
 
+    TextView TxtDetalle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_correo);
+        TxtDetalle = findViewById(R.id.TxtDetalle);
+        TxtDetalle.setText("De: "+getIntent().getExtras().getString("de")+
+                " Asunto: "+getIntent().getExtras().getString("asunto")+
+                " Contenido: "+getIntent().getExtras().getString("texto"));
     }
 }
